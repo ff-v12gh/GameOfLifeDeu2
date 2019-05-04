6291135f70b9d480c7ea21f50431a3e26da8a8c8
Martin Schlenk <msk2wsdic@me.com>
2019-05-04 11:26:31 +0200

    Neighbours-Funktion fertig

--- src/game/GM.java
+++ src/game/GM.java
@@ -24,6 +24,26 @@ public class GM {
 
     }
 
+    public static int neighbours(int x, int y){
+        int count = 0;
+
+        //Anfang mitte rechts, Uhrzeigersinn
+        int[] xoff = {1, 1, 0, -1, -1, -1, 0, 1};
+        int[] yoff = {0, 1, 1, 1, 0, -1, -1, -1};
+
+        for(int i = 0; i<8; i++){
+            try {
+                if(cells[x+xoff[i]][y+yoff[i]]){
+                    count++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return count;
+    }
+
     public static int r(int min, int max){
         return ThreadLocalRandom.current().nextInt(min, max); // Ggf. max + 1
     }
